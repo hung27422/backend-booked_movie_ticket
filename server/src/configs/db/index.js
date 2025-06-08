4b6dfaf4959798b6f4973214a1e64643fa74b2fb
Ho Tan Hung <tanhungho2002@gmail.com>
2025-06-08 20:17:29 +0700

    feat: update MongoDB connection string to use Atlas cluster

--- server/src/configs/db/index.js
+++ server/src/configs/db/index.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-
+//mongodb://127.0.0.1:27017/booked_movies_ticket-dev
 async function connect() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/booked_movies_ticket-dev", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://tanhungho2002:Aa123456789!@booked-movies-ticket-de.0pdhu76.mongodb.net/booked_movies_ticket-dev-clt?retryWrites=true&w=majority&appName=booked-movies-ticket-dev",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("Connect Mongoose successfully!!!");
   } catch (error) {
     console.error("Connect failure!!!", error);
