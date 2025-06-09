195ebe40e89157a7ab5a389775efaacef6b57897
Ho Tan Hung <tanhungho2002@gmail.com>
2025-06-09 12:39:10 +0700

    fix: update port configuration to use environment variable

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
-
+require("dotenv").config();
 const route = require("./src/routes/index");
 const db = require("./src/configs/db/index");
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
