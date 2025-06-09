d06265c1253eba9afaa0d3c269648ea0b559526d
Ho Tan Hung <tanhungho2002@gmail.com>
2025-06-09 11:40:01 +0700

    feat: update serverless-http to version 3.2.0 with major refactor
    - Added support for AWS and Azure providers with cleanup and request handling.
    - Implemented response formatting and header sanitization for both providers.
    - Introduced binary content handling and improved URL decoding.
    - Enhanced request and response classes for better compatibility with serverless environments.
    - Updated package.json and serverless-http.d.ts for new features and types.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const serverless = require("serverless-http");
 const port = 5000;
 
 const route = require("./src/routes/index");
@@ -15,6 +16,7 @@ db.connect();
 // Routes init
 route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+module.exports.handler = serverless(app);
